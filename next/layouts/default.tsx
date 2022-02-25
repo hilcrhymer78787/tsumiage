85b3f6c9f8552b5d6655420f15d2c93b686dc458
今野龍之介 <hilcrhymer78787@gmail.com>
2022-02-25 10:11:22 +0900

    test

--- next/layouts/default.tsx
+++ next/layouts/default.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navigation from "@/components/Navigation";
+import Navigation from "@/components/common/Navigation";
 import Header from "@/components/common/Header";
 import { connect } from "react-redux";
 
