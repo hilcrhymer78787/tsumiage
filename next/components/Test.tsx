9711941bd206aaedfa0ef785f42ee1050843d5fa
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-02 16:38:09 +0900

    test

--- next/components/Test.tsx
+++ next/components/Test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-export default function Sample() {
+export default function Test() {
     const [value, setValue] = React.useState<number>(5);
     return (
         <>
