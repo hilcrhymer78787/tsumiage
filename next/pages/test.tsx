66dc4ba88205f429b0cbbf95eb9def311fbb4d04
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-02 21:25:12 +0900

    test

--- next/pages/test.tsx
+++ next/pages/test.tsx
@@ -1,24 +1,23 @@
 import { connect } from "react-redux";
 import Layout from '@/layouts/default';
-import BasicSelect from '@/components/Test';
+import Test from '@/components/Test';
 const mapStateToProps = (state: any) => {
     return {
         loginInfo: state.loginInfo,
-        count: state.count,
-        post: state.post
     };
 };
-About.getLayout = function getLayout(page) {
+TestPage.getLayout = function getLayout(page) {
     return (
         <Layout>{page}</Layout>
     );
 };
-function About({ loginInfo }) {
+function TestPage({ loginInfo }) {
     return (
         <div>
-            <pre>{JSON.stringify(loginInfo, null, 2)}</pre>
+            <Test />
+            <pre>{JSON.stringify(loginInfo.name, null, 2)}</pre>
         </div>
     );
 }
 
-export default connect(mapStateToProps)(About);
+export default connect(mapStateToProps)(TestPage);
