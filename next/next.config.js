e0e418dcd9dfce682ad8162b4cee15ec28c31bf4
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-04 09:48:02 +0900

    test

--- next/next.config.js
+++ next/next.config.js
@@ -1,10 +1,11 @@
 const withPWA = require("next-pwa");
 module.exports = withPWA({
-//   buildExcludes: [/.*\.js\.map/],
-  reactStrictMode: true,
-  swcMinify: false,
-  pwa: {
-    dest: "public", // swの出力ディレクトリ
-    // runtimeCaching: []
-  },
+    //   buildExcludes: [/.*\.js\.map/],
+    trailingSlash: true,
+    reactStrictMode: true,
+    swcMinify: false,
+    pwa: {
+        dest: "public", // swの出力ディレクトリ
+        // runtimeCaching: []
+    },
 });
