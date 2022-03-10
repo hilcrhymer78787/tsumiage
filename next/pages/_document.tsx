8ea3b4878c51b7d5e6adbf03ff2cd1c24d8b14b9
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-10 15:01:07 +0900

    test

--- next/pages/_document.tsx
+++ next/pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
         return (
             <Html lang="ja-JP" dir="ltr">
                 <Head>
+                    <meta name="viewport" content="width=device-width,initial-scale=1.0,viewport-fit=cover" />
                     {/* windows */}
                     <meta name="msapplication-square70x70logo" content="/site-tile-70x70.png" />
                     <meta name="msapplication-square150x150logo" content="/site-tile-150x150.png" />
