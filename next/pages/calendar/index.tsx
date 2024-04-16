821d2609ff08c7dc0961b9aac5ae3df00a0dbb79
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-04-16 15:26:21 +0900

    fix

--- next/pages/calendar/index.tsx
+++ next/pages/calendar/index.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useMemo } from "react";
+import { useCallback, useEffect } from "react";
 
 import CalendarList from "@/components/calendar/CalendarList";
+import ErrTxt from "@/components/common/ErrTxt";
 import Layout from "@/layouts/default";
+import Loading from "@/components/common/Loading";
 import { loginInfoAtom } from "@/data/user";
 import { useReadWorkMonth } from "@/data/work/useReadWorkMonth";
 import { useRecoilValue } from "recoil";
@@ -10,25 +12,40 @@ import { useRouter } from "next/router";
 const Calendar = () => {
   const router = useRouter();
   const loginInfo = useRecoilValue(loginInfoAtom);
-  const { calendars, readWorkMonth } = useReadWorkMonth();
+  const { calendars, readWorkMonthLoading, readWorkMonthError, readWorkMonth } =
+    useReadWorkMonth();
 
-  const getCalendarData = async (year?: number, month?: number) => {
-    if (!loginInfo?.id) return;
-    await readWorkMonth({
-      userId: loginInfo.id,
-      year: year ?? Number(router.query.year),
-      month: month ?? Number(router.query.month),
-    });
-  };
+  const getCalendarData = useCallback(
+    async (year?: number, month?: number) => {
+      if (!loginInfo?.id) return;
+      await readWorkMonth({
+        userId: loginInfo.id,
+        year: year ?? Number(router.query.year),
+        month: month ?? Number(router.query.month),
+      });
+    },
+    [loginInfo, readWorkMonth, router.query.month, router.query.year]
+  );
 
   useEffect(() => {
     getCalendarData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const CalendarContent = useCallback(() => {
+    if (!!readWorkMonthError) return <ErrTxt txt={readWorkMonthError} />;
+    if (calendars === null) {
+      if (readWorkMonthLoading) return <Loading />;
+      return <></>;
+    }
+    return (
+      <CalendarList calendars={calendars} getCalendarData={getCalendarData} />
+    );
+  }, [readWorkMonthLoading, readWorkMonthError, calendars, getCalendarData]);
+
   return (
     <Layout pcMaxWidth={false}>
-      <CalendarList calendars={calendars} getCalendarData={getCalendarData} />
+      <CalendarContent />
     </Layout>
   );
 };
