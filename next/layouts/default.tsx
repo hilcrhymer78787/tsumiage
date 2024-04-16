ac55f67aacd44f3253f147d97a7b06728fd1e371
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-04-16 11:43:56 +0900

    fix

--- next/layouts/default.tsx
+++ next/layouts/default.tsx
@@ -1,75 +1,53 @@
 import {
+  BottomNavigation,
+  BottomNavigationAction,
   Box,
+  Breakpoint,
   Container,
-  Divider,
   Drawer,
   List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
+  Paper,
 } from "@mui/material";
-import { ReactNode, useEffect } from "react";
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 
 import GroupIcon from "@mui/icons-material/Group";
-import NavItem from "@/components/common/NavItem";
-import Navigation from "@/components/common/Navigation";
+import { ReactNode } from "react";
 import TaskIcon from "@mui/icons-material/Task";
 import TodayIcon from "@mui/icons-material/Today";
 import UserAvatar from "@/components/common/UserAvatar";
 import dayjs from "dayjs";
 import { loginInfoAtom } from "@/data/user";
 import theme from "@/plugins/theme";
+import { useMedia } from "@/data/media/useMedia";
 import { useRecoilValue } from "recoil";
 import { useRouter } from "next/router";
 
 type Props = {
   children: ReactNode;
+  authRequired?: boolean;
+  pcMaxWidth?: Breakpoint;
+  spP?: string;
+  pcP?: string;
 };
-
 export type Nav = {
-  ttl: string;
+  label: string;
   value: string;
   icon: ReactNode;
 };
-function Layout({ children }: Props) {
+const Layout = ({
+  children,
+  pcMaxWidth = "sm",
+  spP = "70px 10px 180px",
+  pcP = "32px 24px",
+}: Props) => {
+  const { isPc } = useMedia();
   const router = useRouter();
   const getColor = (value: string) => {
     return router.pathname === value ? theme.palette.primary.main : "white";
   };
   const loginInfo = useRecoilValue(loginInfoAtom);
 
-  const navs: Nav[] = [
-    {
-      ttl: "task",
-      value: "/task",
-      icon: <TaskIcon />,
-    },
-    {
-      ttl: "calendar",
-      value: "/calendar",
-      icon: <TodayIcon />,
-    },
-    {
-      ttl: "friend",
-      value: "/friend",
-      icon: <GroupIcon />,
-    },
-    {
-      ttl: "mypage",
-      value: "/mypage",
-      icon: (
-        <UserAvatar
-          src={loginInfo?.user_img ?? ""}
-          size={30}
-          onClick={() => {}}
-          borderColor={getColor("/mypage")}
-        />
-      ),
-    },
-  ];
-
-  const onClickNav = ({ value }: Nav) => {
+  const onChangeNav = (value: string) => {
     if (value == "/calendar") {
       router.push({
         pathname: "calendar",
@@ -83,24 +61,54 @@ function Layout({ children }: Props) {
     router.push(value);
   };
 
-  useEffect(() => {
-    if (!loginInfo) router.push("/auth");
-  }, []);
+  const navs: Nav[] = [
+    {
+      label: "task",
+      value: "/task",
+      icon: <TaskIcon />,
+    },
+    {
+      label: "calendar",
+      value: "/calendar",
+      icon: <TodayIcon />,
+    },
+    {
+      label: "friend",
+      value: "/friend",
+      icon: <GroupIcon />,
+    },
+    {
+      label: "mypage",
+      value: "/mypage",
+      icon: (
+        <UserAvatar
+          src={loginInfo?.user_img ?? ""}
+          size={30}
+          onClick={() => {}}
+          borderColor={getColor("/mypage")}
+        />
+      ),
+    },
+  ];
 
-  if (!loginInfo) return <></>;
-  return (
-    <>
+  if (isPc) {
+    return (
       <Box sx={{ display: "flex" }} data-testid="Dashboard">
         <Drawer variant="permanent">
-          <Toolbar>{loginInfo.name}</Toolbar>
-          <Divider />
           <List component="nav">
-            {navs.map((nav, i) => (
-              <ListItemButton key={i} onClick={() => onClickNav(nav)}>
-                <ListItemIcon>{nav.icon}</ListItemIcon>
-                <ListItemText primary={nav.ttl} />
-              </ListItemButton>
-            ))}
+            {navs.map((nav, i) => {
+              const color = getColor(nav.value);
+              return (
+                <ListItemButton
+                  onClick={() => onChangeNav(nav.value)}
+                  data-testid={`NavItem-${i}`}
+                  key={i}
+                >
+                  <ListItemIcon sx={{ color }}>{nav.icon}</ListItemIcon>
+                  <ListItemText sx={{ color }} primary={nav.label} />
+                </ListItemButton>
+              );
+            })}
           </List>
         </Drawer>
         <Box
@@ -112,13 +120,36 @@ function Layout({ children }: Props) {
             paddingLeft: "170px",
           }}
         >
-          <Container maxWidth="lg" sx={{ my: 4 }}>
+          <Container maxWidth={pcMaxWidth} sx={{ my: 0, p: pcP }}>
             {children}
           </Container>
         </Box>
       </Box>
-      <Navigation />
+    );
+  }
+  return (
+    <>
+      <Container sx={{ p: spP }}>{children}</Container>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={router.pathname}
+          onChange={(_, value) => onChangeNav(value)}
+        >
+          {navs.map((nav, i) => (
+            <BottomNavigationAction
+              key={i}
+              label={nav.label}
+              value={nav.value}
+              icon={nav.icon}
+            />
+          ))}
+        </BottomNavigation>
+      </Paper>
     </>
   );
-}
+};
 export default Layout;
