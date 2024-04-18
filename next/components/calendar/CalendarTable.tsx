5188bb0186d12eb959f40ac474bf4805fee878f8
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-04-18 10:53:10 +0900

    fix

--- next/components/calendar/CalendarTable.tsx
+++ next/components/calendar/CalendarTable.tsx
@@ -42,7 +42,7 @@ const CalendarTable = ({ calendars, getCalendarData }: Props) => {
         sx={{
           width: `calc(100vw - ${isPc ? NAV_WIDTH : 0}px)`,
           //TODO 正確な値を計算
-          height: `calc(100vh - ${isPc ? 60 : 200}px)`,
+          height: `calc(100vh - ${isPc ? 60 : 120}px - env(safe-area-inset-bottom))`,
         }}
       >
         <Table stickyHeader sx={{ width: tableWidth }}>
