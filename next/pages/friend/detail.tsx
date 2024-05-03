37ca75ca656b87ed9a14a2ccd3799f260a09453e
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-05-03 15:11:02 +0900

    test

---
+++ next/pages/friend/detail.tsx
@@ -0,0 +1,76 @@
+import { useCallback, useEffect, useMemo } from "react";
+
+import CalendarTable from "@/components/calendar/CalendarTable";
+import ErrTxt from "@/components/common/ErrTxt";
+import Layout from "@/layouts/default";
+import Loading from "@/components/common/Loading";
+import { Typography } from "@mui/material";
+import { useReadWorkMonth } from "@/data/work/useReadWorkMonth";
+import { useRouter } from "next/router";
+
+const FriendId = () => {
+  const router = useRouter();
+  const { calendars, readWorkMonthLoading, readWorkMonthError, readWorkMonth } =
+    useReadWorkMonth();
+
+  const userId = useMemo(() => {
+    return Number(router.query.user?.[0]);
+  }, [router.query.user]);
+
+  const userName = useMemo(() => {
+    return router.query.user?.[1];
+  }, [router.query.user]);
+
+  const year = useMemo(() => {
+    return Number(router.query.year);
+  }, [router.query.year]);
+
+  const month = useMemo(() => {
+    return Number(router.query.month);
+  }, [router.query.month]);
+
+  const getCalendarData = useCallback(
+    async (year?: number, month?: number) => {
+      await readWorkMonth({
+        userId,
+        year: year ?? Number(router.query.year),
+        month: month ?? Number(router.query.month),
+      });
+    },
+    [userId, readWorkMonth, router.query.month, router.query.year]
+  );
+
+  useEffect(() => {
+    getCalendarData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [year, month]);
+
+  const CalendarContent = useCallback(() => {
+    if (!!readWorkMonthError) return <ErrTxt txt={readWorkMonthError} />;
+    if (calendars === null) {
+      if (readWorkMonthLoading) return <Loading />;
+      return <></>;
+    }
+    return (
+      <CalendarTable
+        userName={userName}
+        calendars={calendars}
+        getCalendarData={getCalendarData}
+      />
+    );
+  }, [
+    readWorkMonthLoading,
+    readWorkMonthError,
+    calendars,
+    getCalendarData,
+    userName,
+  ]);
+
+  if (router.asPath === router.route) return null;
+  return (
+    <Layout pcMaxWidth={false} spP="0 !important" pcP="0 !important">
+      <CalendarContent />
+    </Layout>
+  );
+};
+export default FriendId;
