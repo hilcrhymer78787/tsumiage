b6c2386f3a6e5c3b7008f16c263b26cc52d001ca
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-05-11 10:44:24 +0900

    fix

--- next/pages/_document.tsx
+++ next/pages/_document.tsx
@@ -8,6 +8,10 @@ class MyDocument extends Document {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#000000" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,viewport-fit=cover"
+        />
       </Head>
         <body>
           <Main />
