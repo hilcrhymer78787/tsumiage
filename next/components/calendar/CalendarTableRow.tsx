8bf3c38411be7f67fba38a61f39ca77b8be312fe
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-05-17 13:50:15 +0900

    fix

--- next/components/calendar/CalendarTableRow.tsx
+++ next/components/calendar/CalendarTableRow.tsx
@@ -1,13 +1,20 @@
 import {
   Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
   TableCell,
   TableRow,
 } from "@mui/material";
-import { TASK_NAME_WIDTH, stickyStyle } from "@/components/calendar/CalendarTable";
+import {
+  TASK_NAME_WIDTH,
+  stickyStyle,
+} from "@/components/calendar/CalendarTable";
 
 import { Calendar } from "@/data/work/useReadWorkMonth";
 import CalendarTableCell from "@/components/calendar/CalendarTableCell";
 import { Task } from "@/data/task/useReadTasks";
+import { useState } from "react";
 
 type CalendarTableRowProps = {
   task: Task;
@@ -19,15 +26,25 @@ const CalendarTableRow = ({
   calendars,
   readonly,
 }: CalendarTableRowProps) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   return (
     <TableRow>
       <TableCell sx={{ ...stickyStyle, zIndex: 2 }}>
         <Box
+          onClick={() => setIsDialogOpen(true)}
           sx={{ width: `${TASK_NAME_WIDTH}px`, paddingLeft: 1 }}
           className="ellipsis"
         >
           {task.name}
         </Box>
+        <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+          <DialogTitle>{task.name}</DialogTitle>
+          {/* <DialogContent>
+            {process.env.NODE_ENV === "development" && (
+              <pre>{JSON.stringify(calendars, null, 4)}</pre>
+            )}
+          </DialogContent> */}
+        </Dialog>
       </TableCell>
       {calendars.map((calendar) => (
         <TableCell key={calendar.date} align="center">
