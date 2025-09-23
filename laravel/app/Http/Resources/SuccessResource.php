99ad249099c50d58790f2c1c00bd4f496d3e9e29
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-09-23 21:11:24 +0900

    fix

--- laravel/app/Http/Resources/SuccessResource.php
+++ laravel/app/Http/Resources/SuccessResource.php
@@ -1,18 +1,12 @@
 <?php
 // TODO: 場所おかしい？
 
-namespace App\Http\Resources\Base;
+namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SuccessResource extends JsonResource
 {
-    /**
-     * データを配列に変換
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array<string, mixed>
-     */
     public function toArray($request): array
     {
         return [
