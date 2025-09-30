f1458a8fe537dfbcc769f59536013b4c3539d99d
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-09-30 19:17:24 +0900

    fix

--- next/pages/task/sort/index.tsx
+++ next/pages/task/sort/index.tsx
@@ -1,65 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
-import { useReadTasks } from "@/data/task/useReadTasks";
-
-import ErrTxt from "@/components/common/ErrTxt";
 import Layout from "@/layouts/default";
-import Loading from "@/components/common/Loading";
-import NoData from "@/components/common/NoData";
-import Sortable from "@/components/common/Sortable";
-import TaskSortHeader from "@/components/task/TaskSortHeader";
-import dayjs from "dayjs";
-import { useLoginInfo } from "@/data/common/useLoginInfo";
-import { useSortTasks } from "@/data/task/useSortTasks";
-import { Task } from "@/data/types/task";
+import TaskSortMain from "@/components/taskSort/TaskSortMain";
 
 const TaskSort = () => {
-  const { loginInfo } = useLoginInfo();
-  const { tasks, readTasks, isLoading, error } = useReadTasks();
-  const { sortTasks } = useSortTasks();
-  const [scrollY, setScrollY] = useState(0);
-
-  const apiTaskRead = async () => {
-    await readTasks({
-      date: dayjs().format("YYYY-MM-DD"),
-      user_id: loginInfo?.id ?? 0,
-    });
-  };
-
-  const apiTaskSort = async (tasks: Task[]) => {
-    const ids = tasks.map(({ id }) => id);
-    sortTasks({ ids });
-  };
-
-  const TaskSortContent = useCallback(() => {
-    if (!!error) return <ErrTxt txt={error} />;
-    if (tasks === null) {
-      if (isLoading) return <Loading />;
-      return <></>;
-    }
-    if (!tasks.length) return <NoData txt="登録されているタスクはありません" />;
-    return <Sortable initItems={tasks} onChange={apiTaskSort} />;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading, error, tasks]);
-
-  useEffect(() => {
-    apiTaskRead();
-
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <Layout pcP="80px 0" spP="70px 10px 180px">
-      <TaskSortHeader isGray={!!scrollY} />
-      <TaskSortContent />
-      {process.env.NODE_ENV === "development" && <pre>{JSON.stringify(tasks, null, 4)}</pre>}
+      <TaskSortMain />
     </Layout>
   );
 };
